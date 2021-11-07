8400c0bba360f8d8e8bec978b205bca85f599b08
Youssefwael397 <youssefwael397@gmail.com>
2021-11-07 19:14:19 +0200

    connection config

--- conn.php
+++ conn.php
@@ -3,15 +3,17 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
 header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
 
-$hostname="127.0.0.1";
-$username="root";
-$password= "";
-$database = "reg_user";
+$hostname="sql105.epizy.com";
+$username="epiz_30278136";
+$password= "84M0otCwCwobX";
+$database = "epiz_30278136_reg_user";
 $conn;
 try{
+
   $conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
+  echo 'failed to connect to database';
 }
    
    
